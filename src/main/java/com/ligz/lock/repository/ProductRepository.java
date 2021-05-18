9f3020ce620894fbdc1c69d49206274112454138
liguangzheng <380449395@qq.com>
2021-05-18 11:57:26 +0800

    feat: modify pessimistic lock get by product for update

--- src/main/java/com/ligz/lock/repository/ProductRepository.java
+++ src/main/java/com/ligz/lock/repository/ProductRepository.java
@@ -1,10 +1,15 @@
 package com.ligz.lock.repository;
 
-import com.ligz.lock.entity.Order;
 import com.ligz.lock.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    @Query(value = "select * from product where id = :id for update", nativeQuery = true)
+    Optional<Product> getProductByPessimisticLock(@Param("id") Long id);
 }
